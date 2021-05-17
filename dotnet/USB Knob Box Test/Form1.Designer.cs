77130d55ca324a81e76448332c2d1d4e77b00790
Glen Akins <ktmglen@glenakins.com>
2021-05-17 17:48:39 -0600

    add C# dot net app for testing

---
+++ dotnet/USB Knob Box Test/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿
+namespace USB_Knob_Box_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBoxLed1 = new System.Windows.Forms.CheckBox();
+            this.checkBoxLed2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(13, 13);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(375, 56);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(394, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(394, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(13, 75);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(375, 56);
+            this.trackBar2.TabIndex = 2;
+            this.trackBar2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(394, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(13, 137);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(375, 56);
+            this.trackBar3.TabIndex = 4;
+            this.trackBar3.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(394, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(13, 199);
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(375, 56);
+            this.trackBar4.TabIndex = 6;
+            this.trackBar4.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(394, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(13, 261);
+            this.trackBar5.Maximum = 100;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(375, 56);
+            this.trackBar5.TabIndex = 8;
+            this.trackBar5.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(482, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(38, 21);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.TabStop = false;
+            this.checkBox1.Text = "1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(482, 40);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(38, 21);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.TabStop = false;
+            this.checkBox2.Text = "2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(482, 67);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(38, 21);
+            this.checkBox3.TabIndex = 12;
+            this.checkBox3.TabStop = false;
+            this.checkBox3.Text = "3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(482, 94);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(38, 21);
+            this.checkBox4.TabIndex = 13;
+            this.checkBox4.TabStop = false;
+            this.checkBox4.Text = "4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(482, 121);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(38, 21);
+            this.checkBox5.TabIndex = 14;
+            this.checkBox5.TabStop = false;
+            this.checkBox5.Text = "5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(482, 148);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(38, 21);
+            this.checkBox6.TabIndex = 15;
+            this.checkBox6.TabStop = false;
+            this.checkBox6.Text = "6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(482, 175);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(38, 21);
+            this.checkBox7.TabIndex = 16;
+            this.checkBox7.TabStop = false;
+            this.checkBox7.Text = "7";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(482, 202);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(38, 21);
+            this.checkBox8.TabIndex = 17;
+            this.checkBox8.TabStop = false;
+            this.checkBox8.Text = "8";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxLed1
+            // 
+            this.checkBoxLed1.AutoSize = true;
+            this.checkBoxLed1.Location = new System.Drawing.Point(482, 243);
+            this.checkBoxLed1.Name = "checkBoxLed1";
+            this.checkBoxLed1.Size = new System.Drawing.Size(65, 21);
+            this.checkBoxLed1.TabIndex = 18;
+            this.checkBoxLed1.Text = "LED1";
+            this.checkBoxLed1.UseVisualStyleBackColor = true;
+            this.checkBoxLed1.CheckedChanged += new System.EventHandler(this.checkBoxLed1_CheckedChanged);
+            // 
+            // checkBoxLed2
+            // 
+            this.checkBoxLed2.AutoSize = true;
+            this.checkBoxLed2.Location = new System.Drawing.Point(553, 243);
+            this.checkBoxLed2.Name = "checkBoxLed2";
+            this.checkBoxLed2.Size = new System.Drawing.Size(65, 21);
+            this.checkBoxLed2.TabIndex = 19;
+            this.checkBoxLed2.Text = "LED2";
+            this.checkBoxLed2.UseVisualStyleBackColor = true;
+            this.checkBoxLed2.CheckedChanged += new System.EventHandler(this.checkBoxLed2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 314);
+            this.Controls.Add(this.checkBoxLed2);
+            this.Controls.Add(this.checkBoxLed1);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "USB Knob Box Test";
+            this.Load += new System.EventHandler(this.FormLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBoxLed1;
+        private System.Windows.Forms.CheckBox checkBoxLed2;
+    }
+}
+
---
+++ dotnet/USB Knob Box Test/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HidLibrary;
+
+namespace USB_Knob_Box_Test
+{
+    public partial class Form1 : Form
+    {
+        private static HidDevice _device;
+        public delegate void ReadHandlerDelegate(HidReport report);
+        int oldKeys;
+        int leds;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLoad(object sender, EventArgs e)
+        {
+            // use 0x0014 for EFM8UB2 version; use 0x0015 for PIC18 version
+            _device = HidDevices.Enumerate(0x4247, 0x0015).FirstOrDefault();
+
+            if (_device == null)
+            {
+                MessageBox.Show("Unable to find the USB device.", "Device Not Found", MessageBoxButtons.OK);
+            }
+
+            _device.OpenDevice();
+            _device.MonitorDeviceEvents = true;
+            _device.ReadReport(ReadProcess);
+
+            label1.Text = "0";
+            label2.Text = "0";
+            label3.Text = "0";
+            label4.Text = "0";
+            label5.Text = "0";
+
+            oldKeys = 0;
+            leds = 0;
+
+            UpdateLeds();
+        }
+
+        private void ReadProcess(HidReport report)
+        {
+            BeginInvoke(new ReadHandlerDelegate(ReadHandler), new object[] { report });
+        }
+
+        private void ReadHandler(HidReport report)
+        {
+            if (report.ReportId == 0x01)
+            {
+                int keys = report.Data[0];
+                int enc1 = report.Data[1];
+                int enc2 = report.Data[2];
+                int enc3 = report.Data[3];
+                int enc4 = report.Data[4];
+                int enc5 = report.Data[5];
+
+                if ((enc1 & 1) == 1)
+                {
+                    if (trackBar1.Value < 10)
+                    {
+                        trackBar1.Value++;
+                        label1.Text = trackBar1.Value.ToString();
+                    }
+                } else if ((enc1 & 2) == 2)
+                {
+                    if (trackBar1.Value > 0)
+                    {
+                        trackBar1.Value--;
+                        label1.Text = trackBar1.Value.ToString();
+                    }
+                }
+
+                if ((enc2 & 1) == 1)
+                {
+                    if (trackBar2.Value < 10)
+                    {
+                        trackBar2.Value++;
+                        label2.Text = trackBar2.Value.ToString();
+                    }
+                }
+                else if ((enc2 & 2) == 2)
+                {
+                    if (trackBar2.Value > 0)
+                    {
+                        trackBar2.Value--;
+                        label2.Text = trackBar2.Value.ToString();
+                    }
+                }
+
+                if ((enc3 & 1) == 1)
+                {
+                    if (trackBar3.Value < 10)
+                    {
+                        trackBar3.Value++;
+                        label3.Text = trackBar3.Value.ToString();
+                    }
+                }
+                else if ((enc3 & 2) == 2)
+                {
+                    if (trackBar3.Value > 0)
+                    {
+                        trackBar3.Value--;
+                        label3.Text = trackBar3.Value.ToString();
+                    }
+                }
+
+                if ((enc4 & 1) == 1)
+                {
+                    if (trackBar4.Value < 10)
+                    {
+                        trackBar4.Value++;
+                        label4.Text = trackBar4.Value.ToString();
+                    }
+                }
+                else if ((enc4 & 2) == 2)
+                {
+                    if (trackBar4.Value > 0)
+                    {
+                        trackBar4.Value--;
+                        label4.Text = trackBar4.Value.ToString();
+                    }
+                }
+
+                if ((enc5 & 1) == 1)
+                {
+                    if (trackBar5.Value < 100)
+                    {
+                        trackBar5.Value++;
+                        label5.Text = trackBar5.Value.ToString();
+                    }
+                }
+                else if ((enc5 & 2) == 2)
+                {
+                    if (trackBar5.Value > 0)
+                    {
+                        trackBar5.Value--;
+                        label5.Text = trackBar5.Value.ToString();
+                    }
+                }
+
+                checkBox1.Checked = checkBox1.Checked ^ (((keys & 1) != 0) && ((oldKeys & 1) == 0));
+                checkBox2.Checked = checkBox2.Checked ^ (((keys & 2) != 0) && ((oldKeys & 2) == 0));
+                checkBox3.Checked = checkBox3.Checked ^ (((keys & 4) != 0) && ((oldKeys & 4) == 0));
+                checkBox4.Checked = checkBox4.Checked ^ (((keys & 8) != 0) && ((oldKeys & 8) == 0));
+                checkBox5.Checked = checkBox5.Checked ^ (((keys & 16) != 0) && ((oldKeys & 16) == 0));
+                checkBox6.Checked = checkBox6.Checked ^ (((keys & 32) != 0) && ((oldKeys & 32) == 0));
+                checkBox7.Checked = checkBox7.Checked ^ (((keys & 64) != 0) && ((oldKeys & 64) == 0));
+                checkBox8.Checked = checkBox8.Checked ^ (((keys & 128) != 0) && ((oldKeys & 128) == 0));
+
+                oldKeys = keys;
+            }
+            _device.ReadReport(ReadProcess);
+        }
+
+        private void checkBoxLed1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxLed1.Checked)
+            {
+                leds |= 0x01;
+            }
+            else
+            {
+                leds &= ~0x01;
+            }
+            UpdateLeds();
+        }
+
+        private void checkBoxLed2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxLed2.Checked)
+            {
+                leds |= 0x02;
+            } else
+            {
+                leds &= ~0x02;
+            }
+            UpdateLeds();
+        }
+
+        private void UpdateLeds ()
+        {
+            var outData = _device.CreateReport();
+            outData.ReportId = 0x02;
+            outData.Data[0] = (byte)leds;
+            _device.WriteReport(outData);
+        }
+
+    }
+}
